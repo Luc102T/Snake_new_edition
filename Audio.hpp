bb21af48095508d6f6d1f51e8e7faf48efe993a8
Hoang Luc <luchoang22062006@gmail.com>
2025-03-22 23:55:16 +0700

    Thêm hệ thống âm thanh Audio

---
+++ Audio.hpp
@@ -0,0 +1,25 @@
+#pragma once 
+#include "Game.hpp" 
+#include <SDL2/SDL_mixer.h>
+
+class Audio {
+private:
+	Mix_Chunk* chunk = NULL;
+	Mix_Music* music = NULL;
+	int volume; 
+
+public:
+	Audio(); 
+	~Audio(); 
+	
+	void loadSound(const char* path);  
+	void loadMusic(const char* path); 
+	void playSound();
+	void playSound(const int& v);   
+	void playMusic();
+	void playMusic(const int& v); 
+	bool isPlaying(); 
+	void pause(); 
+	void resume();    
+}; 
+
